1ffeb3d93444b1e6caa5749f4908237e7af60340
Bilal Quadri <bilalquadri92@gmail.com>
2012-05-09 03:56:52 -0400

    Added more samples for dictionarie

--- python/dict.py
+++ python/dict.py
@@ -8,3 +8,10 @@ for key in reversed(sorted(mahJSON)):
   #min 0
   #max 10
   #judgeCount 3
+
+
+dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+#Output: {'sape': 4139, 'jack': 4098, 'guido': 4127}
+
+dict(sape=4139, guido=4127, jack=4098)
+#Output: {'sape': 4139, 'jack': 4098, 'guido': 4127}
