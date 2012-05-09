234784aa707a8537d52c76d1aca6275e12097e98
Bilal Quadri <bilalquadri92@gmail.com>
2012-05-09 03:40:32 -0400

    Added dictionary sample

---
+++ python/dict.py
@@ -0,0 +1,10 @@
+mahJSON = {'name': 'score', 'min': 0, 'max': 10, 'judgeCount': 3}
+
+for key in reversed(sorted(mahJSON)):
+  print key, mahJSON[key]
+
+#Output:
+  #name score
+  #min 0
+  #max 10
+  #judgeCount 3
